d043275ccf2b7c93fa8934968e34ce618bc36e4f
JoePrinzevalli <joeprinzevalli123@gmail.com>
2022-01-23 21:10:46 -0500

    start of project

---
+++ project-files/js/script.js
@@ -0,0 +1,33 @@
+/************************************************
+Treehouse Techdegree:
+Project 4 - Random Quote Generator
+*************************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community
+
+/*** 
+ * `quotes` array 
+***/
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
